27150306e79191fe3c95eff269576792f65a75d0
Golden Silence <47517371+Golden-Silence@users.noreply.github.com>
2020-10-12 22:51:49 +0200

    initial commit

---
+++ OcelotApiGateway/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+               .UseKestrel()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .ConfigureAppConfiguration((hostingContext, config) =>
+               {
+                   config
+                       .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                       .AddJsonFile("appsettings.json", true, true)
+                       .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                       .AddJsonFile("ocelot.json")
+                       .AddEnvironmentVariables();
+               })
+               .ConfigureServices(s => {
+                   s.AddOcelot();
+               })
+               .ConfigureLogging((hostingContext, logging) =>
+               {
+                   //add your logging
+               })
+               .UseIISIntegration()
+               .Configure(app =>
+               {
+                   app.UseWebSockets();
+                   app.UseOcelot().Wait();
+               })
+               .Build()
+               .Run();
+        }
+    }
+}
